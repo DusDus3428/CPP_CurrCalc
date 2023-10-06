4cfa2fc169dded752c83b05d8f1f317261362e48
DusDus3428 <dustinnoah.young@gmail.com>
2023-10-07 11:13:45 +1300

    Changed test for currency conversion

--- CurrCalc/test/currency_detail/currency_detail_test.cpp
+++ CurrCalc/test/currency_detail/currency_detail_test.cpp
@@ -6,8 +6,8 @@
 class CurrencyDetailTestFixture : public ::testing::Test {
     protected: 
         void SetUp() {
-            exchangeRates = {ExchangeRate("GBP", 2.00000), ExchangeRate("EUR", 3.00000)};
-            currencyDetail = new CurrencyDetail("USD", exchangeRates);
+            exchangeRates = {ExchangeRate("GBP", 0.87000), ExchangeRate("USD", 1.06000)};
+            currencyDetail = new CurrencyDetail("EUR", exchangeRates);
         }
 
         std::vector<ExchangeRate> exchangeRates;
@@ -16,5 +16,5 @@ class CurrencyDetailTestFixture : public ::testing::Test {
 
 TEST_F(CurrencyDetailTestFixture, ConvertAmount)
 {
-    EXPECT_EQ(currencyDetail->convertAmount(500.0, "GBP"), 1000.0);
+    EXPECT_EQ(currencyDetail->convertAmount(500.0, "USD", "GBP"), 410.37735);
 }
